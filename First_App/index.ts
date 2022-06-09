d07eadd273b723d616015595761f1e18cb375bc7
John Sims <jwsims816@gmail.com>
2022-06-08 22:32:15 -0500

    type annotations added

--- First_App/index.ts
+++ First_App/index.ts
@@ -15,9 +15,14 @@ axios.get(url).then(response => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed)
+
+})
+
+const logTodo = (id:number, title:string, completed:boolean) => {
   console.log(`
   The Todo with ID ${id}
   Has a title of: ${title}
   Is it finished? ${completed}
   `)
-})
+}
