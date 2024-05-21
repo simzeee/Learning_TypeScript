b24791d44dc10857d323b70edf7a9dcd729c6edc
simzeee <jwsims816@gmail.com>
2024-05-20 22:02:21 -0400

    delete old courses, start TypeHero, finish primitive

--- CodeWars/dragons.ts
+++
@@ -1,6 +0,0 @@
-const BULLETS_PER_DRAGON: number = 2;
-
-function hero(bullets: number, dragons: number): boolean {
-  const survive: boolean = bullets >= dragons * BULLETS_PER_DRAGON;
-  return survive;
-}
--- FEM_TypeScript/src/objects_examples.js
+++
@@ -1,9 +0,0 @@
-var car;
-/**
- * Print information about a car to the console
- * @param car - the car to print
- */
-function printCar(car) {
-    console.log("".concat(car.make, " ").concat(car.model, " (").concat(car.year, ")"));
-}
-printCar({ make: 'test', model: 'test', year: 0 });
