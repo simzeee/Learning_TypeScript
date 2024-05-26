21f8bad5f5a26bf764096aa133783439d544aaad
simzeee <jwsims816@gmail.com>
2024-05-26 16:06:00 -0400

    finish twofer and resistor color and update .gitignore

---
+++ Exercism/typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,16 @@
+export const colorCode = (color: string): number => {
+  return COLORS.indexOf(color);
+};
+
+export const COLORS: string[] = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
--- Exercism/typescript/two-fer/two-fer.ts
+++ Exercism/typescript/two-fer/two-fer.ts
@@ -2,7 +2,7 @@
  * This stub is provided to make it straightforward to get started.
  */
 
-export function twoFer(): string {
+export function twoFer(name: string = "you"): string {
   // ^                 ^   ^ this is called a return type; it's the type of the
   // ^                 ^     value that is returned from this function
   // ^                 ^
@@ -10,4 +10,5 @@ export function twoFer(): string {
   // ^
   // allows the tests to import this function and call it
   // <-- Your code goes here. You may remove all the commentary in this file.
+  return `One for ${name}, one for me.`
 }
